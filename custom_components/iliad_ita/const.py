c6bfb39785e1f85a8d3b43d388a03fad1145707d
Roberto <62454469+robertovendrame@users.noreply.github.com>
2026-08-24 22:10:21 +0200

    Add configurable data thresholds and update interval

--- custom_components/iliad_ita/const.py
+++ custom_components/iliad_ita/const.py
@@ -3,15 +3,23 @@
 from datetime import timedelta
 
 DOMAIN = "iliad_ita"
-PLATFORMS = ["sensor", "button"]
+PLATFORMS = ["sensor", "binary_sensor", "button"]
 
 CONF_NAME = "name"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
+CONF_DATA_THRESHOLD_GB = "data_threshold_gb"
+CONF_DATA_THRESHOLD_PERCENT = "data_threshold_percent"
+CONF_UPDATE_INTERVAL_HOURS = "update_interval_hours"
 
 DEFAULT_NAME = "SIM Iliad"
+DEFAULT_DATA_THRESHOLD_GB = 10.0
+DEFAULT_DATA_THRESHOLD_PERCENT = 10.0
+DEFAULT_UPDATE_INTERVAL_HOURS = 6
+MIN_UPDATE_INTERVAL_HOURS = 1
+MAX_UPDATE_INTERVAL_HOURS = 24
 
 LOGIN_URL = "https://www.iliad.it/account/login"
 CONSUMPTION_URL = "https://www.iliad.it/account/consumi-e-credito"
 
-UPDATE_INTERVAL = timedelta(hours=6)
+UPDATE_INTERVAL = timedelta(hours=DEFAULT_UPDATE_INTERVAL_HOURS)
