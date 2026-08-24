50008f2023ad80a0c941217d79aea0addb8b22df
Roberto <62454469+robertovendrame@users.noreply.github.com>
2026-08-24 16:18:27 +0200

    Add Iliad constants

---
+++ custom_components/iliad_ita/const.py
@@ -0,0 +1,17 @@
+"""Constants for the Iliad Italia integration."""
+
+from datetime import timedelta
+
+DOMAIN = "iliad_ita"
+PLATFORMS = ["sensor"]
+
+CONF_NAME = "name"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+DEFAULT_NAME = "SIM Iliad"
+
+LOGIN_URL = "https://www.iliad.it/account/login"
+CONSUMPTION_URL = "https://www.iliad.it/account/consumi-e-credito"
+
+UPDATE_INTERVAL = timedelta(hours=6)
